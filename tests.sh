33225977db3bd2da56b23131a7c5e5f9e5325202
Colin Pitrat <colin.pitrat@gmail.com>
2026-05-06 17:09:24 +0100

    Add some (manual) test cases to quickly verify that things are not completely broken

---
+++ tests.sh
@@ -0,0 +1,14 @@
+# Houses roof in London (shouldn't be transparent)
+cargo run -- display gamedata/gta-uk/UK.CMP gamedata/gta-uk/Style001.g24 --camera-position 212.59,6.07,263.88 --camera-rotation 2.04,-26.48,-0.00 --no-lids-transparency
+
+# Hospital sign in London
+cargo run -- display gamedata/gta-uk/UK.CMP gamedata/gta-uk/Style001.g24 --camera-position 199.45,6.07,204.78 --camera-rotation 91.83,-24.43,0.00 --no-lids-transparency
+
+# Docks sign in Liberty City
+cargo run -- display gamedata/gta/NYC.CMP gamedata/gta/STYLE001.G24 --camera-position 234.69,2.46,54.64 --camera-rotation 97.08,-31.54,0.00
+
+# Water and shades in London
+cargo run -- display gamedata/gta-uk/UK.CMP gamedata/gta-uk/Style001.g24 --camera-position 100.18,6.07,169.29 --camera-rotation 47.86,-26.48,0.00 --no-lids-transparency
+
+# Water and shades in Liberty City
+cargo run -- display gamedata/gta/NYC.CMP gamedata/gta/STYLE001.G24 --camera-position 234.69,2.46,54.64 --camera-rotation 161.54,-28.38,-0.00
